651fdd6563ea2c49157350dca87476771a9a8364
Pablo <pablofpastor@gmail.com>
2015-02-17 18:35:59 -0600

    Both files updated and seem to be working

---
+++ makeCacheMatrix.R
@@ -0,0 +1,34 @@
+## These two functions caches the inverse of a matrix 
+## and then calculates, on cacheSolve the inverse of a square matrix
+
+## This first function creates a special matrix which does the following:
+## 1. Set the value of the matrix
+## 2. Gets the value of the matrix
+## 3. Sets the value for the inverse
+## 4. Gets the value for the inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+        inverse <- NULL
+        set <- function(y){
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) inverse <<- inv
+        getinv <- function() inverse
+        list(set=set,get=get,setinv=setinv,getinv=getinv)
+}
+
+## This function calculates the inverse of the matrix returned by makeCacheMatrix
+
+cacheSolve <- function(x, ...) {
+                m <- x$getinv()
+                if(!is.null(m)) {
+                        message("getting cached data")
+                        return(m)
+                }
+                data <- x$get()
+                m <- solve(data, ...)
+                x$setinv(m)
+                m
+}
